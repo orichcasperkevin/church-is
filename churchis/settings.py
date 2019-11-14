9b8373c461e6ecb23a30ebd153a713c6da335f39
casper <orichcasper@gmail.com>
2019-11-15 01:18:18 +0300

    heroku setup

--- churchis/settings.py
+++ churchis/settings.py
@@ -187,6 +187,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
