bd8e275ecb7d57cae3a5be7c18e764d6d51cae37
casper <orichcasper@gmail.com>
2019-12-15 11:06:19 +0300

    setup database for tenant

--- Clients/forms.py
+++ Clients/forms.py
@@ -1,21 +1,30 @@
 from django import forms
 
 class TryDemoForm(forms.Form):
-    name = forms.CharField(max_length=30,
-                            widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'enter your full names'}))
+    first_name = forms.CharField(max_length=30,
+                            widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'enter your first name'}))
+
+    last_name = forms.CharField(max_length=30,
+                                widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'enter your last name'}))
+
     email = forms.EmailField(max_length=254,
                             widget = forms.EmailInput(attrs={'class':'form-control', 'placeholder':'your email'}))
 
     def clean(self):
         cleaned_data = super(TryDemoForm, self).clean()
-        name = cleaned_data.get('name')
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
         email = cleaned_data.get('email')
-        if not name and not email:
-            raise forms.ValidationError('You have to write something!')
+        if not first_name and not last_name and not email:
+            raise forms.ValidationError('all Fields are required')
 
 class getAnvilForm(forms.Form):
-        name = forms.CharField(max_length=30,
-                                widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'enter your full names'}))
+        first_name = forms.CharField(max_length=30,
+                                widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'enter your first name'}))
+
+        last_name = forms.CharField(max_length=30,
+                                    widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'enter your last name'}))
+
         email = forms.EmailField(max_length=254,
                                 widget = forms.EmailInput(attrs={'class':'form-control', 'placeholder':'your email'}))
 
@@ -24,9 +33,12 @@ class getAnvilForm(forms.Form):
 
         location = forms.CharField(max_length=254,
                                         widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'describe the location of your church'}))
+
         def clean(self):
             cleaned_data = super(getAnvilForm, self).clean()
-            name = cleaned_data.get('name')
+            first_name = cleaned_data.get('first_name')
+            last_name = cleaned_data.get('last_name')
+            nameOfChurch = cleaned_data.get('nameOfChurch')
             email = cleaned_data.get('email')
-            if not name and not email:
-                raise forms.ValidationError('You have to write something!')
+            if not first_name and not last_name and not email and not nameOfChurch:
+                raise forms.ValidationError('All fields are required')
--- Clients/views.py
+++ Clients/views.py
@@ -1,30 +1,55 @@
 import random
 
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from tenant_schemas.utils import schema_context
+
 from .forms import TryDemoForm,getAnvilForm
+from member.models import Member
 from .models import Client
+
+STARTER_PASSWORD = "changeMe"
+
+def setupDemoDatabase(first_name,last_name,email,demo_name):
+    #inside the demo schema
+    with schema_context(demo_name):
+        #create member
+        username = first_name.lower() + last_name.lower()
+        print(username)
+        user = User(first_name=first_name, username=username, last_name=last_name, email=email)
+        user.set_password(STARTER_PASSWORD)
+        user.save()
+        print(user)
+        user_id = user.id
+
+        member = Member.objects.create(member_id = user_id)
+        print(member)
+
 
 def index(request):
     if request.method == 'POST':
         demo_form = TryDemoForm(request.POST)
         get_anvil_form = getAnvilForm(request.POST)
         try:
+            #try using the get anvil form
             if getAnvilForm.is_valid(request.POST):
                 print("ouhs")
         except:
+            #use the demo form
             if demo_form.is_valid():
-                name = demo_form.cleaned_data['name']
+                first_name = demo_form.cleaned_data['first_name']
+                last_name = demo_form.cleaned_data['last_name']
                 email = demo_form.cleaned_data['email']
                 #demo name
                 demo_name = "demo" + str(random.choice(range(10000000)))
-                print(demo_name)
                 #domain_url
                 domain_url = demo_name +"."+ request.get_host().split(':')[0]
                 print(domain_url)
-
                 tenant = Client(domain_url=domain_url, schema_name=demo_name,
-                                name=name,paid_until='2014-12-05',on_trial=True)
-                tenant.save() # migrate_schemas automatically called, your tenant is ready to be used!
+                                name=first_name,paid_until='2014-12-05',on_trial=True)
+                tenant.save()
+                setupDemoDatabase(first_name,last_name,email,demo_name)
+
 
     else:
         demo_form = TryDemoForm()
