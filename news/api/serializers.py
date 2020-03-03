9a0fcc89061ad85ebc322caef779a8a21dfdfc29
casper <orichcasper@gmail.com>
2020-03-03 09:53:43 +0300

    remove church groups from news

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -6,7 +6,7 @@ from news.models import (News, )
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
-        fields = ('id','heading','church_group','article',
-                  'date', 'author', 'website')
+        fields = ('id','heading','article',
+                  'date', 'author')
         depth = 2
         extra_kwargs = {'id': {'read_only': True}}
--- news/models.py
+++ news/models.py
@@ -10,12 +10,9 @@ class News(models.Model):
         News published by the church
     '''
     heading = models.CharField(max_length=100)
-    featured_image = models.ImageField(upload_to='news/', null=True, blank=True)
-    church_group = models.ManyToManyField(ChurchGroup, blank=True)
     article = RichTextField()
     date = models.DateTimeField(auto_now_add=True)
-    author = models.CharField(max_length=100, default="The church")
-    website = models.BooleanField(default=True)
+    author = models.CharField(max_length=100, default="The church")    
 
     class Meta:
         ordering = ('-date',)
